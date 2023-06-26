a64e622d1971539ec9943f19230dab925d2dd129
aniiaham <aniya_axm@icloud.com>
2023-06-26 19:43:07 -0400

    fourth commit

--- components/scheduletour.tsx
+++ components/scheduletour.tsx
@@ -6,21 +6,24 @@ const ScheduleTour = () => {
   return (
     <section id="team" className="h-screen w-full">
       <div className="flex flex-col py-10 mx-auto container">
-        <div className="flex flex-row w-full">
-          <span className="text-9xl text-gray-500 font-extrabold h-full">
-            02
-          </span>
-          <div className="flex flex-col justify-end pb-3">
-            <HText>CLIMB</HText>
-            <div className="h-5 w-5 bg-slate-700/20" />
+        <div className="flex flex-row w-full gap-6">
+          <div className="flex">
+            <span className="text-9xl text-gray-500 font-extrabold h-full">
+              02
+            </span>
+            <div className="flex flex-col justify-end pb-4">
+              <HText>CLIMB</HText>
+              <div className="h-5 w-5 bg-slate-700/20" />
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col items-center">
-          <p className="max-w-3xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in
-            ante viverra, rutrum erat rutrum, consectetur mi. Proin at maximus
-            est. Nullam purus ex.
-          </p>
+
+          <div className="flex flex-col items-center justify-center">
+            <p className="max-w-3xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in
+              ante viverra, rutrum erat rutrum, consectetur mi. Proin at maximus
+              est. Nullam purus ex.
+            </p>
+          </div>
         </div>
       </div>
       <div className="bg-blue-900/90 w-full flex flex-col items-start p-4">
@@ -40,7 +43,9 @@ const ScheduleTour = () => {
           className="-z-50"
         />
         <div>
-            
+          <div className="h-60 w-80 bg-slate-700/20">
+            <HText>SCHEDULE</HText>
+          </div>
         </div>
       </div>
     </section>
